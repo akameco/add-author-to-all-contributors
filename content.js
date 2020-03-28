46e5a99c27ca16bf51efc101509b7c476119576f
akameco <akameco.t@gmail.com>
2020-03-28 11:48:36 +0900

    refactor: use $

--- content.js
+++ content.js
@@ -2,15 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const $ = document.querySelector.bind(document);
   const button = `
     <div class="discussion-sidebar-item">
-      <button class="btn btn-sm btn-block mt-2 all-contributor" type="button">
+      <button id="all-contributor" class="btn btn-sm btn-block mt-2" type="button">
           Add contributor
       </button> 
     </div>
   `;
-  const sidebarEl = $("#partial-discussion-sidebar");
-  sidebarEl.insertAdjacentHTML("beforeend", button);
-  const buttonEl = sidebarEl.querySelector(".all-contributor");
-  buttonEl.addEventListener(
+  $("#partial-discussion-sidebar").insertAdjacentHTML("beforeend", button);
+  $("#all-contributor").addEventListener(
     "click",
     () => {
       const fieldEl = $("#new_comment_field");
